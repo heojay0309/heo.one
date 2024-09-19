75528ab9d9011a56dcbd65be226c63e010fff342
heojay0309 <heojay0309@gmail.com>
2024-09-19 13:08:26 -0700

    update

--- src/components/WorkTimeline.tsx
+++ src/components/WorkTimeline.tsx
@@ -46,15 +46,15 @@ const WorkTimeline = ({
           : ''
       } ${mobileHidden ? 'hidden laptop:flex' : ''}`}
     >
-      <div className="flex flex-col  gap-[32px] laptop:p-[32px] p-[8px] justify-center h-full">
+      <div className="flex flex-col laptop:p-[32px] p-[16px] justify-center h-full">
         {Object.keys(timeline).map((time, index) => {
           return (
             <div
               key={index}
-              className="flex w-full flex-col relative justify-center items-start p-[4px]"
+              className="flex w-full flex-col relative justify-center items-start h-full"
             >
-              <div className="flex justify-between gap-[16px] items-center">
-                <div className="flex flex-col opacity-80">
+              <div className="flex justify-evenly w-full gap-[8px] p-[8px] items-center h-full ">
+                <div className="flex flex-col opacity-80 justify-center ">
                   <div className="text-[8px] leading-[12px]">
                     {timeline[time].month}
                   </div>
@@ -63,22 +63,28 @@ const WorkTimeline = ({
                     {timeline[time].year}
                   </div>
                 </div>
-                <div className="p-[1px] relative">
-                  <div className="w-[16px] h-[16px] rounded-full border-white border flex items-center justify-center">
-                    <div className="w-[12px] h-[12px] rounded-full bg-white opacity-40"></div>
-                    <div
-                      className={`${
-                        index !== Object.keys(timeline).length - 1
-                          ? ''
-                          : 'hidden'
-                      } h-[32px] flex items-center justify-center absolute top-[32px]`}
-                    >
-                      <div className="h-[16px] border-white border-l border-opacity-10" />
+                <div className="p-[1px] relative flex flex-col items-center justify-center ">
+                  <div className="w-[16px] h-[16px] rounded-full border-white border flex items-center justify-center flex-shrink-0">
+                    <div className="w-[12px] h-[12px] rounded-full bg-white opacity-40 "></div>
+                  </div>
+                  <div
+                    className={`${
+                      index !== Object.keys(timeline).length - 1
+                        ? 'visible'
+                        : 'invisible'
+                    } flex items-center absolute justify-center laptop:h-[64px] tablet:h-[48px] tablet:top-[32px] top-[40px] h-[64px] flex-1`}
+                  >
+                    <div className={`w-full absolute h-full flex-1 `}>
+                      <div className="border-white border-l border-opacity-10 h-full" />
                     </div>
                   </div>
                 </div>
-                <div className="flex flex-col flex-wrap p-1">
-                  <div className="text-[12px] leading-[16px]">{time}</div>
+                <div className="flex flex-col justify-betewen flex-wrap items-start w-full gap-[4px]">
+                  <div className="flex flex-wrap h-full">
+                    <div className="text-[12px] leading-[16px] text-wrap">
+                      {time}
+                    </div>
+                  </div>
                   <span className=" text-wrap text-[8px] leading-[12px]">
                     {timeline[time].title}
                   </span>
