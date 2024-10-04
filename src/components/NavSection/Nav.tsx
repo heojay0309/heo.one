df4218662534486f577a873853d1acd20e2b9559
heojay0309 <heojay0309@gmail.com>
2024-10-03 19:44:49 -0700

    delete hamburger menu

--- src/components/NavSection/Nav.tsx
+++ src/components/NavSection/Nav.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState, useEffect, useRef } from 'react';
-import NavDrop from './NavDrop';
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect, useRef } from "react";
+import NavDrop from "./NavDrop";
 
 const Navbar = () => {
   const [menuToggle, setMenuToggle] = useState<boolean>(false);
@@ -17,19 +17,19 @@ const Navbar = () => {
 
   const handleOutsideClick = (event: MouseEvent) => {
     const target = event.target as HTMLElement;
-    if (!target.closest('.menu-button') && !target.closest('.menu-dropdown')) {
+    if (!target.closest(".menu-button") && !target.closest(".menu-dropdown")) {
       setMenuToggle(false);
     }
   };
 
   useEffect(() => {
     if (menuToggle && !hoveringDropdown) {
-      document.addEventListener('click', handleOutsideClick);
+      document.addEventListener("click", handleOutsideClick);
     } else {
-      document.removeEventListener('click', handleOutsideClick);
+      document.removeEventListener("click", handleOutsideClick);
     }
     return () => {
-      document.removeEventListener('click', handleOutsideClick);
+      document.removeEventListener("click", handleOutsideClick);
     };
   }, [menuToggle, hoveringDropdown]);
 
@@ -50,85 +50,15 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full flex items-center relative justify-center laptop:px-[64px] tablet:px-[32px] h-[64px] px-[16px] z-10">
+    <div className="relative z-10 flex h-[64px] w-full items-center justify-center px-[16px] tablet:px-[32px] laptop:px-[64px]">
       <Link
-        href={'/'}
-        className="text-[16px] font-black leading-[24px] tracking-[0.12em] flex items-center jsutify-center self-center "
+        href={"/"}
+        className="jsutify-center flex items-center self-center text-[16px] font-black leading-[24px] tracking-[0.12em]"
       >
         JAY HEO
       </Link>
-      {/* HOVER HAMBURGER MENU */}
-      <button
-        onClick={handleClick}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        className={`flex flex-col absolute right-0 laptop:mr-[64px] tablet:mr-[32px] mr-[16px] gap-[8px] justify-center items-center w-[40px] h-[40px]`}
-      >
-        <div
-          className={`w-[24px] rounded-[20px] border-[0.5px] border-white relative transition-transform  ${
-            menuToggle && 'translate-y-[-4px]'
-          } duration-300`}
-        />
-        <Image
-          src={'/navIcons/arrowMenu.svg'}
-          alt={'arrow'}
-          width={8}
-          height={10}
-          className={` w-1/3 absolute ease-in-out h-1/2 transition-all duration-300  ${
-            menuToggle ? 'opacity-100' : 'opacity-0'
-          }  before:-rotate-90 after:rotate-90`}
-        />
-        <div
-          className={`w-[24px] rounded-[20px] border-[0.5px] ease-in-out border-white  ${
-            menuToggle ? 'opacity-0' : 'opacity-100'
-          } duration-100 transition-opacity`}
-        />
-        <div
-          className={`w-[24px] rounded-[20px] border-[0.5px] border-white  ${
-            menuToggle && 'translate-y-[4px]'
-          } duration-300`}
-        />
-        {/* HOVER DROPDOWN MENU */}
-        <div
-          onMouseEnter={handleMouseEnter}
-          onMouseMove={handleMouseEnter}
-          className={`${
-            menuToggle || hoveringDropdown
-              ? 'opacity-100 visible'
-              : 'opacity-0 collapse'
-          } absolute top-[56px] right-[20px] backdrop-blur opacity-0 delay-300 duration-500 transition-all ease-in-out`}
-        >
-          <NavDrop handleMouseEnter={handleMouseEnter} />
-        </div>
-      </button>
     </div>
   );
 };
 
 export default Navbar;
-{
-  /* <button
-onClick={handleClick}
-onMouseEnter={() => setMenuToggle(true)}
-// onMouseLeave={() => setMenuToggle(false)}
-className={`hidden tablet:flex group menu-button flex-col absolute right-0 laptop:mr-[64px] tablet:mr-[32px] mr-[16px] gap-[8px] justify-center items-center w-[40px] h-[40px]`}
->
-<div className="w-[24px] rounded-[20px] border-[0.5px] border-white relative transition-transform group-hover:translate-y-[-4px] duration-300 " />
-<Image
-  src={'/navIcons/arrowMenu.svg'}
-  alt={'arrow'}
-  width={8}
-  height={10}
-  className="opacity-0 w-1/3 absolute ease-in-out h-1/2  transition-all duration-300 group-hover:opacity-100 before:-rotate-90 after:rotate-90"
-/>
-<div className="w-[24px] rounded-[20px] border-[0.5px] ease-in-out border-white group-hover:opacity-0 duration-100 transition-opacity" />
-<div className="w-[24px] rounded-[20px] border-[0.5px] border-white group-hover:translate-y-[4px] duration-300" />
-<div
-  onMouseEnter={() => setMenuToggle(true)}
-  className={`absolute top-[64px] right-[20px] group-hover:visible opacity-0 delay-150 group-hover:opacity-100 collapse duration-500 transition-all  ease-in-out`}
->
-  <NavDrop />
-</div>
-</button>
- */
-}
