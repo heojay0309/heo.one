65a1a90a3bc0393e8fdb959e62414bac4c7129a1
heojay0309 <heojay0309@gmail.com>
2024-10-21 13:47:17 -0700

    image width change

--- src/components/Introduction/Intro.tsx
+++ src/components/Introduction/Intro.tsx
@@ -5,17 +5,17 @@ import Link from "next/link";
 const Intro = () => {
   return (
     <div className="text-align -z-10 flex h-full min-h-[640px] w-full snap-start flex-col items-center gap-[32px] overflow-x-clip tablet:min-h-[864px] tablet:flex-row tablet:justify-around laptop:gap-[64px]">
-      <div className="relative h-[270px] w-[300px] overflow-clip rounded-bl-[16px] rounded-br-[200px] rounded-tl-[16px] rounded-tr-[16px] tablet:h-[480px] tablet:w-[400px] tablet:min-w-[400px] tablet:rounded-br-[307px]">
+      <div className="relative h-[270px] w-[300px] overflow-clip rounded-bl-[16px] rounded-br-[200px] rounded-tl-[16px] rounded-tr-[16px] tablet:h-[480px] tablet:w-[480px] tablet:min-w-[400px] tablet:rounded-br-[307px]">
         <Image
           src={"/selfie.png"}
           alt={"me"}
           priority
           fill
-          className="absolute object-cover"
+          className="absolute object-cover opacity-90"
         />
       </div>
       <div className="flex h-full flex-col gap-[16px] tablet:max-h-[400px] tablet:max-w-[568px] tablet:justify-start">
-        <div className="h-full w-full tablet:max-h-[240px]">
+        <div className="h-full w-full bg-white tablet:max-h-[240px]">
           <h1 className="text-[48px] font-[600] leading-[64px]">Hello!</h1>
           <span className="text-[16px] font-[500] leading-[24px] tablet:text-[20px] tablet:leading-[32px]">
             My name is <strong>Jay Heo</strong>, I&apos;m a detail-oriented{" "}
