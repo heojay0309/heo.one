1190f86356497b9bc7d967024f738b278ac089c5
heojay0309 <heojay0309@gmail.com>
2024-11-09 13:17:46 -0800

    update sidebar

--- src/components/NavSection/SideMenu.tsx
+++ src/components/NavSection/SideMenu.tsx
@@ -14,91 +14,11 @@ const SideMenu = ({ activeSection }: SideMenuProps) => {
   };
   return (
     <div
-      className={`fixed top-[184px] hidden h-full tablet:right-[8px] tablet:block tablet:gap-[64px] laptop:right-[64px]`}
+      className={`fixed top-[490px] hidden h-full tablet:right-[4px] tablet:block tablet:gap-[64px] laptop:right-[64px]`}
     >
       <div
-        className={`${path === "/resume" && "hidden"} flex flex-col items-center gap-[16px] rounded-lg bg-white bg-opacity-30 hover:bg-opacity-40 tablet:gap-[16px] tablet:px-[8px] tablet:py-[16px]`}
+        className={`${path === "/resume" && "hidden"} transition-opacity duration-500 ${activeSection === "intro" ? "opacity-0" : "opacity-100"} flex flex-col items-center gap-[16px] rounded-lg bg-white bg-opacity-10 hover:bg-opacity-20 tablet:gap-[16px] tablet:px-[8px] tablet:py-[16px]`}
       >
-        {/* <Link
-          href={"#intro"}
-          className="flex h-[36px] w-[36px] items-center justify-center rounded-full bg-black bg-opacity-0 hover:bg-opacity-5 active:bg-opacity-10 tablet:h-[40px] tablet:w-[40px]"
-        >
-          <Image
-            src={"/new/icons/home.svg"}
-            alt={"home"}
-            height={24}
-            width={24}
-            className="h-[22px] w-[22px] select-none tablet:h-[24px] tablet:w-[24px]"
-          />
-        </Link>
-        <div className="w-[24px] border border-black border-opacity-10 tablet:w-[40px]"></div>
-        <Link
-          href={"#ShineResumes"}
-          className="flex h-[36px] w-[36px] items-center justify-center rounded-full bg-black bg-opacity-0 hover:bg-opacity-5 active:bg-opacity-10 tablet:h-[40px] tablet:w-[40px]"
-        >
-          <Image
-            src={"/new/icons/project_shine.svg"}
-            alt={"ShineResumes"}
-            height={32}
-            width={32}
-            className="h-[22px] w-[22px] select-none tablet:h-[24px] tablet:w-[24px]"
-          />
-        </Link>
-        <div className="w-[24px] border border-black border-opacity-10 tablet:w-[40px]"></div>
-        <Link
-          href={"#Preps"}
-          className="flex h-[36px] w-[36px] items-center justify-center rounded-full bg-black bg-opacity-0 hover:bg-opacity-5 active:bg-opacity-10 tablet:h-[40px] tablet:w-[40px]"
-        >
-          <Image
-            src={"/new/icons/project_preps.svg"}
-            alt={"Preps AI"}
-            height={32}
-            width={32}
-            className="h-[24px] w-[24px] select-none tablet:h-[24px] tablet:w-[32px]"
-          />
-        </Link>
-        <div className="w-[24px] border border-black border-opacity-10 tablet:w-[40px]"></div>
-
-        <Link
-          href={"#Mustadd"}
-          className="flex h-[36px] w-[36px] items-center justify-center rounded-full bg-black bg-opacity-0 hover:bg-opacity-5 active:bg-opacity-10 tablet:h-[40px] tablet:w-[40px]"
-        >
-          <Image
-            src={"/new/icons/project_mustadd.svg"}
-            alt={"Mustadd"}
-            height={40}
-            width={40}
-            className="h-[28px] w-[28px] select-none tablet:h-[36px] tablet:w-[36px]"
-          />
-        </Link>
-        <div className="w-[24px] border border-black border-opacity-10 tablet:w-[40px]"></div>
-
-        <Link
-          href={"#Giverr"}
-          className="flex h-[36px] w-[36px] items-center justify-center rounded-full bg-black bg-opacity-0 hover:bg-opacity-5 active:bg-opacity-10 tablet:h-[40px] tablet:w-[40px]"
-        >
-          <Image
-            src={"/new/icons/project_giverr.svg"}
-            alt={"Giverr"}
-            height={32}
-            width={32}
-            className="h-[22px] w-[22px] select-none tablet:h-[26px] tablet:w-[26px]"
-          />
-        </Link>
-        <div className="w-[24px] border border-black border-opacity-10 tablet:w-[40px]"></div>
-
-        <Link
-          href={"#Mellow"}
-          className="flex h-[36px] w-[36px] items-center justify-center rounded-full bg-black bg-opacity-0 hover:bg-opacity-5 active:bg-opacity-10 tablet:h-[40px] tablet:w-[40px]"
-        >
-          <Image
-            src={"/new/icons/project_mellow.svg"}
-            alt={"Mellow"}
-            height={24}
-            width={32}
-            className="h-[24px] w-[24px] select-none tablet:h-[26px] tablet:w-[32px]"
-          />
-        </Link> */}
         <Link
           href={"#intro"}
           className={`flex h-[36px] w-[36px] items-center justify-center rounded-full bg-black ${isActive("intro")} hover:bg-opacity-5 active:bg-opacity-10 tablet:h-[40px] tablet:w-[40px]`}
