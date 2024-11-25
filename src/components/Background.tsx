ec8776456853ee6ec6f23ae7cf64dce4d66d9c06
heojay0309 <heojay0309@gmail.com>
2024-11-25 11:40:29 -0800

    puzzle

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,7 +1,36 @@
 const PuzzleBackground = () => {
   return (
-    <div className="puzzle-piece fixed right-[-64px] top-[0px] h-[240px] w-[240px] scale-[25%] border-[#e2e8f0] tablet:right-[64px] tablet:top-[160px] tablet:scale-90">
-      {}
+    <div className="absolute left-[8%] top-[20%] opacity-5 tablet:scale-125">
+      <div className="flex">
+        <div className="z-10 flex flex-col">
+          <div className="puzzle relative overflow-x-clip">
+            <div className="circle bg-[#2d3748]"></div>
+            <div className="circle bg-[#2d3748]"></div>
+            <div className="top"></div>
+            <div className="bottom"></div>
+          </div>
+          <div className="puzzle relative rotate-90 overflow-x-clip">
+            <div className="circle bg-[#2d3748]"></div>
+            <div className="circle bg-[#2d3748]"></div>
+            <div className="top"></div>
+            <div className="bottom"></div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="puzzle relative z-20 rotate-90 overflow-x-clip">
+            <div className="circle bg-[#2d3748]"></div>
+            <div className="circle bg-white"></div>
+            <div className="top"></div>
+            <div className="bottom"></div>
+          </div>
+          <div className="puzzle relative rotate-180 overflow-x-clip">
+            <div className="circle bg-[#2d3748]"></div>
+            <div className="circle bg-[#2d3748]"></div>
+            <div className="top"></div>
+            <div className="bottom"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
